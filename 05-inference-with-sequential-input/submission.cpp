bcbe5717b9d19bdc456d90a61726e8649253b46b
ShawnHymel <5232145+ShawnHymel@users.noreply.github.com>
2022-09-10 23:25:11 -0500

    Added raw test samples to sequential exercise

--- 05-inference-with-sequential-input/submission.cpp
+++ 05-inference-with-sequential-input/submission.cpp
@@ -65,7 +65,7 @@ static const float means[] = {0.4869, -0.6364, 8.329, -0.1513, 4.631, -9.8836};
 static const float std_devs[] = {3.062, 7.2209, 6.9951, 61.3324, 104.1638, 108.3149};
 
 // Store raw readings in a buffer that has 6 * 100 = 600 elements
-static float input_buf[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE];
+static float input_buf[NUM_CHANNELS * NUM_READINGS];
 
 // Wrapper for raw input buffer
 static signal_t sig;
@@ -107,49 +107,19 @@ void loop() {
 
     // Sample the IMU for 1 second. You should end up with 100 readings for each 
     // of the 6 channels after the 1 second is over.
-    //  - Make sure you wait between each reading long enough for a 100 Hz 
-    //    sampling rate.
+    //  - Convert accelerometer raw readings from G-force to m/s^2
     //  - For each reading, perform standardization using the mean and std_dev
     //    associated with each channel
     //  - Store your standardized readings in input_buf[]
     //  - Recall that the order of input_buf[] should be 
     //    [acc_x0, acc_y0, acc_z0, gyr_x0, gyr_y0, gyr_z0, acc_x1, ...]
+    //  - Make sure you wait between each reading long enough for a 100 Hz 
+    //    sampling rate.
+    //  - Hint: this looks similar to the loop you wrote for the data collection
+    //    exercise (the differences are that you don't need to collect the 
+    //    timestamps and you need to perform standardization on the readings)
     // --- YOUR CODE HERE ---
-    for (int i = 0; i < NUM_READINGS; i++) {
 
-        // Take timestamp so we can hit our target frequency
-        timestamp = millis();
-
-        // Get raw readings from the accelerometer and gyroscope
-        IMU.readAcceleration(acc_x, acc_y, acc_z);
-        IMU.readGyroscope(gyr_x, gyr_y, gyr_z);
-
-        // Convert accelerometer units from G to m/s^s
-        acc_x *= CONVERT_G_TO_MS2;
-        acc_y *= CONVERT_G_TO_MS2;
-        acc_z *= CONVERT_G_TO_MS2;
-
-        // Perform standardization on each reading
-        // Use the values from means[] and std_devs[]
-        acc_x = (acc_x - means[0]) / std_devs[0];
-        acc_y = (acc_y - means[1]) / std_devs[1];
-        acc_z = (acc_z - means[2]) / std_devs[2];
-        gyr_x = (gyr_x - means[3]) / std_devs[3];
-        gyr_y = (gyr_y - means[4]) / std_devs[4];
-        gyr_z = (gyr_z - means[5]) / std_devs[5];
-
-        // Fill input_buf with the standardized readings. Recall tha the order
-        // is [acc_x0, acc_y0, acc_z0, gyr_x0, gyr_y0, gyr_z0, acc_x1, ...]
-        input_buf[(NUM_CHANNELS * i) + 0] = acc_x;
-        input_buf[(NUM_CHANNELS * i) + 1] = acc_y;
-        input_buf[(NUM_CHANNELS * i) + 2] = acc_z;
-        input_buf[(NUM_CHANNELS * i) + 3] = gyr_x;
-        input_buf[(NUM_CHANNELS * i) + 4] = gyr_y;
-        input_buf[(NUM_CHANNELS * i) + 5] = gyr_z;
-
-        // Wait just long enough for our sampling period
-        while (millis() - timestamp < SAMPLING_PERIOD_MS);
-    }
     // --- END CODE ---
 
     // Turn off LED to show we're done recording
@@ -157,9 +127,9 @@ void loop() {
     digitalWrite(LED_R_PIN, HIGH);
 #endif
 
-    // Call run_classifier() to perform preprocessing and inferece
+    // Call run_classifier() to perform preprocessing and inference
     // --- YOUR CODE HERE ---
-    res = run_classifier(&sig, &result, false);
+    
     // --- END CODE ---
 
     // Find the label with the highest classification value
@@ -172,12 +142,7 @@ void loop() {
     float max_val = 0.0;
     int max_idx = -1;
     // --- YOUR CODE HERE ---
-    for (int i = 0; i < NUM_CLASSES; i++) {
-        if (result.classification[i].value > max_val) {
-            max_val = result.classification[i].value;
-            max_idx = i;
-        }
-    }
+    
     // --- END CODE ---
 
     // Print return code and how long it took to perform inference
